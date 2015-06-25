7bc1738763a21e51ae3f99ed67b2edb633a3b408
uzmanj <uzman.jacob@gmail.com>
2015-06-24 20:09:22 -0400

    devise users & pages scaffolded

---
+++ app/views/pages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @page, :id, :name, :url, :created_at, :updated_at
---
+++ db/migrate/20150624234811_create_pages.rb
@@ -0,0 +1,10 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.text :name
+      t.text :url
+
+      t.timestamps null: false
+    end
+  end
+end
